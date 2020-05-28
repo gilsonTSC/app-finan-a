7d5c18d45d456cade5564d45bca9c9139424863a
Gilson <gilson.costa@dce.ufpb.br>
2020-05-28 09:16:50 -0300

    Conectando o cadastro usuário com a API REST

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -4,6 +4,9 @@ import Card from '../components/card'
 import FormGroup from '../components/form-group'
 import {withRouter} from 'react-router-dom'
 
+import UsuarioService from '../app/service/usuarioService'
+import {mensagemSucesso, mensagemErro} from '../components/toastr'
+
 class CadastroUsuario extends React.Component{
 
     state = {
@@ -13,8 +16,24 @@ class CadastroUsuario extends React.Component{
         senhaRepeticao: ''
     }
 
+    constructor(){
+        super();
+        this.service = new UsuarioService();
+    }
+
     cadastrar = () => {
-        console.log(this.state)
+        const usuario = {
+            nome: this.state.nome,
+            email: this.state.email,
+            senha: this.state.senha
+        }
+        this.service.salvar(usuario)
+            .then(response =>{
+                mensagemSucesso('Usuário cadastrado com sucesso! Faça o login para acessar o sistema.')
+                this.props.history.push('/login')
+            }).catch(error => {
+                mensagemErro(error.response.data)
+            })
     }
 
     cancelar = () => {
